6c4a7538515a88e5081200f5cf1ca6551340b3c9
truongne <truonggolang@gmail.com>
2023-10-23 19:22:51 +0700

    rabbitmq

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,33 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './services/cloudinary.service';
 import { CloudinaryController } from './controllers/cloudinary.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 @Module({
     imports: [
-        // RabbitMQModule.forRoot(RabbitMQModule, {
-        //     exchanges: [
-        //         {
-        //             name: 'healthline.upload.folder',
-        //             type: 'direct'
-        //         }
-        //     ],
-        //     uri: process.env.RABBITMQ_URL,
-        //     connectionInitOptions: { wait: true, reject: true, timeout: 2000 },
-        // }),
-        ClientsModule.register([
-            {
-                name: 'MATH_SERVICE',
-                transport: Transport.RMQ,
-                options: {
-                    urls: [process.env.RABBITMQ_URL],
-                    queue: 'cats_queue',
-                    queueOptions: {
-                        durable: false,
-                    },
+        RabbitMQModule.forRoot(RabbitMQModule, {
+            exchanges: [
+                {
+                    name: 'healthline.upload.folder',
+                    type: 'direct'
                 }
-            }
-        ])
+            ],
+            uri: process.env.RABBITMQ_URL,
+            connectionInitOptions: { wait: true, reject: true, timeout: 2000 },
+        }),
     ],
     controllers: [CloudinaryController],
     providers: [CloudinaryService]
--- src/cloudinary/controllers/cloudinary.controller.ts
+++ src/cloudinary/controllers/cloudinary.controller.ts
@@ -4,14 +4,14 @@ import { CloudinaryService } from "../services/cloudinary.service";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { DoctorGuard } from "../../auth/guards/doctor.guard";
 import { UserGuard } from "../../auth/guards/user.guard";
-import { ClientProxy } from "@nestjs/microservices";
+import { AmqpConnection } from "@golevelup/nestjs-rabbitmq";
 
 @ApiTags('CLOUDINARY')
 @Controller()
 export class CloudinaryController {
     constructor(
         private readonly cloudinaryService: CloudinaryService,
-        @Inject('MATH_SERVICE') private readonly client: ClientProxy
+        private readonly amqpConnection: AmqpConnection
     ) { }
 
 
@@ -128,11 +128,11 @@ export class CloudinaryController {
 
         const data = await this.cloudinaryService.uploadFile(file, '/healthline/users/' + req.user.id + '/records/' + folder)
 
-        // const rabbimq = await this.amqpConnection.publish(
-        //     'healthline.upload.folder',
-        //     'upload',
-        //     { data, user: req.user.id, folder: folder }
-        // )
+        const rabbimq = await this.amqpConnection.publish(
+            'healthline.upload.folder',
+            'upload',
+            { data, user: req.user.id, folder: folder }
+        )
         return data
 
     }
@@ -176,11 +176,5 @@ export class CloudinaryController {
 
         const path = 'healthline/users/' + req.user.id + '/records' + folder + '/' + public_id
         return await this.cloudinaryService.deleteFile(path)
-    }
-
-    @Put('test')
-    async test() {
-        const result = await this.client.send<any>('haha', { "one": true, "two": "data" }).toPromise()
-        return result
     }
 }
