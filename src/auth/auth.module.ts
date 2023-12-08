80f376e7ef74b077918372646e21f8313bcca7c7
nhatminhpham772 <nhatminh772002@gmail.com>
2023-12-08 11:52:31 +0700

    add admin guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DoctorStrategy } from './strategies/doctor.strategy';
 import { UserStrategy } from './strategies/user.strategy';
-import { AdminGuard } from './guards/admin.guard';
+import { AdminStrategy } from './strategies/admin.strategy';
 
 @Module({
     providers: [
         DoctorStrategy,
         UserStrategy,
-        AdminGuard
+        AdminStrategy
     ],
 })
 export class AuthModule { }
